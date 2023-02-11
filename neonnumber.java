5a72d309428be3a8aed1dc372bac026493cd2822
20P31A04C1 <84897802+20P31A04C1@users.noreply.github.com>
2023-02-11 21:01:31 +0530

    Add files via upload

---
+++ neonnumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        double d = math.sqrt(n);
+		  int i;
+        while(i<n)
+        {
+        sum=sum+i;
+        i++;
+        }
+        if(add==sum)
+           System.out.print("Neon Number");
+         else
+            System.out.print("Not Neon Number");
+          sc.close();
+   }
+}
